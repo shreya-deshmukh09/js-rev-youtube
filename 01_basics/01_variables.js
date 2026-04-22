98989df69142c4b576dd027d8c411c20b0300ada
shreya-deshmukh09 <deshmukhshreya009@gmail.com>
2026-04-22 19:35:44 +0000

    variables & datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "shreya@gmail.com"
+var accountPassword = "12345" 
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 // not allowed
+
+console.log(accountId);
+
+accountEmail ="hetegj@hc.com"
+accountPassword =56898
+accountCity ="Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var bez of issue in block scope for  functional scope
+   */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3 ) // we are using nodejs, not browser
+
+let name = "shreya" //string//
+let age = 18 //number//
+let isLoggedIn = true //boolean//
+
+// number => 2^53 
+// bigint  if num is too large
+//string=>""
+// boolean => true/false
+//null=> standalone value
+// undefined =>
+// symbol (to find uniqueness)    
+
+
+//objects
+
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
