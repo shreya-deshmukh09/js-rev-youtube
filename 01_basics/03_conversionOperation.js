eb6b6b5dbcadbf97d1dedbe91406e1240178a9f1
shreya-deshmukh09 <deshmukhshreya009@gmail.com>
2026-04-23 06:57:05 +0000

    converson of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+let score = false
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc"=>NaN
+
+
+let isLoggedIn ="SHREYA"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true; 0=>false;
+//""=> false
+//"SHREYA" =>true
+
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
