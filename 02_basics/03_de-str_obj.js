6efead7325563b25ce3a3365c96aba42acc288f2
shreya-deshmukh09 <deshmukhshreya009@gmail.com>
2026-05-22 10:44:36 +0000

    more abt objects

---
+++ 02_basics/03_de-str_obj.js
@@ -0,0 +1,14 @@
+const course = {
+    corsename: "JAVA Script",
+    price:999,
+    courseInstructor:"Hitesh"
+}
+
+course.courseInstructor
+
+const {courseInstructor: Instructor} = course
+
+//console.log(courseInstructor);
+console.log(Instructor);
+
+
