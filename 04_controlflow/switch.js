304ea30a7b87b51ea2f7bc24ce6483ad81e4b1fe
shreya-deshmukh09 <deshmukhshreya009@gmail.com>
2026-05-24 13:08:52 +0000

    switch statement

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// break is imp bcoz if we dont break the case then all cases after true case are executed except default
