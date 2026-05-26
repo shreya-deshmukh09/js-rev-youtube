4e88de97cbea9ae41fc53a10a1104c2deb204d62
shreya-deshmukh09 <deshmukhshreya009@gmail.com>
2026-05-26 12:21:49 +0000

    while & do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,47 @@
+// while loop
+
+//let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let newArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < newArray.length) {
+    console.log(`Value is ${newArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+// do while loop
+
+//let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let Arr = 0
+while (Arr < myArray.length) {
+    console.log(`Value is ${myArray[Arr]}`);
+    Arr = Arr + 1
+}
+
+
+// do while loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+// in "do while" loop condition is checked at the end of loop
+// therefore it executes the code at least one time before
+// checking the condition
